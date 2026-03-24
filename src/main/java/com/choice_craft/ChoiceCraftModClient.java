dcfd4c02f11deb3e72dece181c155e6ff4832370
Holly Yi <hollyyiishere@gmail.com>
2026-03-24 21:03:32 +0800

    Initial Commit

--- src/main/java/com/choice_craft/ChoiceCraftModClient.java
+++ src/main/java/com/choice_craft/ChoiceCraftModClient.java
@@ -1,10 +1,11 @@
 package com.choice_craft;
 
+import com.choice_craft.client.ChoiceCraftClientNetworking;
 import net.fabricmc.api.ClientModInitializer;
 
 public class ChoiceCraftModClient implements ClientModInitializer {
-    @Override
-    public void onInitializeClient() {
-
-    }
+	@Override
+	public void onInitializeClient() {
+		ChoiceCraftClientNetworking.initialize();
+	}
 }

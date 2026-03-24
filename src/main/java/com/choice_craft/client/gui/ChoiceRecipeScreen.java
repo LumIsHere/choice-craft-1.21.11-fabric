878cd1be4d3afb9a80f82718748428524c4c1cf6
Holly Yi <hollyyiishere@gmail.com>
2026-03-24 21:47:14 +0800

    something new

--- src/main/java/com/choice_craft/client/gui/ChoiceRecipeScreen.java
+++ src/main/java/com/choice_craft/client/gui/ChoiceRecipeScreen.java
@@ -77,7 +77,7 @@ public class ChoiceRecipeScreen extends Screen {
 
 	@Override
 	public void render(DrawContext context, int mouseX, int mouseY, float delta) {
-		this.renderBackground(context, mouseX, mouseY, delta);
+		context.fill(0, 0, this.width, this.height, 0x88000000);
 		context.drawCenteredTextWithShadow(this.textRenderer, this.title, this.width / 2, 5, Colors.WHITE);
 
 		super.render(context, mouseX, mouseY, delta);

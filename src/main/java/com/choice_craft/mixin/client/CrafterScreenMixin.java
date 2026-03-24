bd04fd28a2043d4de3bf33ac514ec06bcf224fe3
Holly Yi <hollyyiishere@gmail.com>
2026-03-24 21:13:44 +0800

    Initial Commit

--- src/main/java/com/choice_craft/mixin/client/CrafterScreenMixin.java
+++ src/main/java/com/choice_craft/mixin/client/CrafterScreenMixin.java
@@ -1,13 +1,12 @@
 package com.choice_craft.mixin.client;
 
 import com.choice_craft.client.ChoiceCraftClientState;
+import com.choice_craft.client.gui.widget.ChoiceIconButtonWidget;
 import com.choice_craft.network.payload.RequestChoiceRecipeOptionsPayload;
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
-import net.minecraft.client.gui.screen.ButtonTextures;
 import net.minecraft.client.gui.screen.ingame.CrafterScreen;
 import net.minecraft.client.gui.screen.ingame.HandledScreen;
-import net.minecraft.client.gui.widget.ButtonWidget;
-import net.minecraft.client.gui.widget.TexturedButtonWidget;
+import net.minecraft.client.gui.widget.ClickableWidget;
 import net.minecraft.screen.CrafterScreenHandler;
 import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
@@ -20,15 +19,18 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(CrafterScreen.class)
 public abstract class CrafterScreenMixin extends HandledScreen<CrafterScreenHandler> {
 	@Unique
-	private static final ButtonTextures choice_craft$BUTTON_TEXTURES = new ButtonTextures(
-		Identifier.of("choice_craft", "widget/recipe_switch_button"),
-		Identifier.of("choice_craft", "widget/recipe_switch_button"),
-		Identifier.of("choice_craft", "widget/recipe_switch_button"),
-		Identifier.of("choice_craft", "widget/recipe_switch_button")
-	);
+	private static final int choice_craft$REFRESH_INTERVAL = 10;
 
 	@Unique
-	private ButtonWidget choice_craft$recipeButton;
+	private static final Identifier choice_craft$BUTTON_TEXTURE = Identifier.of("choice_craft", "textures/gui/sprites/widget/recipe_switch_button.png");
+
+	@Unique
+	private static final Identifier choice_craft$BUTTON_HOVERED_TEXTURE = Identifier.of("choice_craft", "textures/gui/sprites/widget/recipe_switch_button_hovered.png");
+
+	@Unique
+	private ClickableWidget choice_craft$recipeButton;
+	@Unique
+	private int choice_craft$refreshTicks;
 
 	protected CrafterScreenMixin(CrafterScreenHandler handler, net.minecraft.entity.player.PlayerInventory inventory, Text title) {
 		super(handler, inventory, title);
@@ -36,9 +38,41 @@ public abstract class CrafterScreenMixin extends HandledScreen<CrafterScreenHand
 
 	@Inject(method = "init", at = @At("TAIL"))
 	private void choice_craft$addButton(CallbackInfo ci) {
-		this.choice_craft$recipeButton = this.addDrawableChild(new TexturedButtonWidget(this.x + 138, this.y + 20, 8, 8, choice_craft$BUTTON_TEXTURES, button -> {
+		this.choice_craft$recipeButton = this.addDrawableChild(new ChoiceIconButtonWidget(this.x + 138, this.y + 20, 8, 8, choice_craft$BUTTON_TEXTURE, choice_craft$BUTTON_HOVERED_TEXTURE, Text.literal("Choose recipe"), button -> {
+			this.choice_craft$requestRecipeOptions(true);
+		}));
+		this.choice_craft$updateButton();
+		this.choice_craft$requestRecipeOptions(false);
+	}
+
+	@Inject(method = "render", at = @At("TAIL"))
+	private void choice_craft$refreshButton(net.minecraft.client.gui.DrawContext context, int mouseX, int mouseY, float deltaTicks, CallbackInfo ci) {
+		this.choice_craft$refreshTicks++;
+		if (this.choice_craft$refreshTicks >= choice_craft$REFRESH_INTERVAL) {
+			this.choice_craft$refreshTicks = 0;
+			this.choice_craft$requestRecipeOptions(false);
+		}
+
+		this.choice_craft$updateButton();
+	}
+
+	@Unique
+	private void choice_craft$updateButton() {
+		if (this.choice_craft$recipeButton == null) {
+			return;
+		}
+
+		boolean visible = ChoiceCraftClientState.get(this.handler.syncId).options().size() > 1;
+		this.choice_craft$recipeButton.visible = visible;
+		this.choice_craft$recipeButton.active = visible;
+	}
+
+	@Unique
+	private void choice_craft$requestRecipeOptions(boolean openAfterResponse) {
+		if (openAfterResponse) {
 			ChoiceCraftClientState.requestOpen(this.handler.syncId);
-			ClientPlayNetworking.send(new RequestChoiceRecipeOptionsPayload(this.handler.syncId));
-		}, Text.literal("Choose recipe")));
+		}
+
+		ClientPlayNetworking.send(new RequestChoiceRecipeOptionsPayload(this.handler.syncId));
 	}
 }
--- src/main/java/com/choice_craft/mixin/client/RecipeBookScreenMixin.java
+++ src/main/java/com/choice_craft/mixin/client/RecipeBookScreenMixin.java
@@ -2,13 +2,12 @@ package com.choice_craft.mixin.client;
 
 import com.choice_craft.client.ChoiceCraftClientState;
 import com.choice_craft.client.gui.ChoiceRecipeScreen;
+import com.choice_craft.client.gui.widget.ChoiceIconButtonWidget;
 import com.choice_craft.network.payload.RequestChoiceRecipeOptionsPayload;
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
-import net.minecraft.client.gui.screen.ingame.HandledScreen;
-import net.minecraft.client.gui.screen.ButtonTextures;
 import net.minecraft.client.gui.screen.ingame.RecipeBookScreen;
-import net.minecraft.client.gui.widget.ButtonWidget;
-import net.minecraft.client.gui.widget.TexturedButtonWidget;
+import net.minecraft.client.gui.screen.ingame.HandledScreen;
+import net.minecraft.client.gui.widget.ClickableWidget;
 import net.minecraft.screen.AbstractCraftingScreenHandler;
 import net.minecraft.screen.AbstractFurnaceScreenHandler;
 import net.minecraft.screen.AbstractRecipeScreenHandler;
@@ -24,15 +23,18 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(RecipeBookScreen.class)
 public abstract class RecipeBookScreenMixin<T extends AbstractRecipeScreenHandler> extends HandledScreen<T> {
 	@Unique
-	private static final ButtonTextures choice_craft$BUTTON_TEXTURES = new ButtonTextures(
-		Identifier.of("choice_craft", "widget/recipe_switch_button"),
-		Identifier.of("choice_craft", "widget/recipe_switch_button"),
-		Identifier.of("choice_craft", "widget/recipe_switch_button"),
-		Identifier.of("choice_craft", "widget/recipe_switch_button")
-	);
+	private static final int choice_craft$REFRESH_INTERVAL = 10;
 
 	@Unique
-	private ButtonWidget choice_craft$recipeButton;
+	private static final Identifier choice_craft$BUTTON_TEXTURE = Identifier.of("choice_craft", "textures/gui/sprites/widget/recipe_switch_button.png");
+
+	@Unique
+	private static final Identifier choice_craft$BUTTON_HOVERED_TEXTURE = Identifier.of("choice_craft", "textures/gui/sprites/widget/recipe_switch_button_hovered.png");
+
+	@Unique
+	private ClickableWidget choice_craft$recipeButton;
+	@Unique
+	private int choice_craft$refreshTicks;
 
 	protected RecipeBookScreenMixin(T handler, net.minecraft.entity.player.PlayerInventory inventory, Text title) {
 		super(handler, inventory, title);
@@ -52,19 +54,29 @@ public abstract class RecipeBookScreenMixin<T extends AbstractRecipeScreenHandle
 
 		int buttonX = this.x + outputSlot.x + 4;
 		int buttonY = this.y + outputSlot.y - 10;
-		this.choice_craft$recipeButton = this.addDrawableChild(new TexturedButtonWidget(buttonX, buttonY, 8, 8, choice_craft$BUTTON_TEXTURES, button -> {
+		this.choice_craft$recipeButton = this.addDrawableChild(new ChoiceIconButtonWidget(buttonX, buttonY, 8, 8, choice_craft$BUTTON_TEXTURE, choice_craft$BUTTON_HOVERED_TEXTURE, Text.literal("Choose recipe"), button -> {
 			if (furnaceHandler) {
-				ChoiceCraftClientState.requestOpen(this.handler.syncId);
-				ClientPlayNetworking.send(new RequestChoiceRecipeOptionsPayload(this.handler.syncId));
+				this.choice_craft$requestRecipeOptions(true);
 			} else if (ChoiceCraftClientState.get(this.handler.syncId).options().size() > 1) {
 				this.client.setScreen(new ChoiceRecipeScreen((RecipeBookScreen<?>) (Object) this, this.handler.syncId));
 			}
-		}, Text.literal("Choose recipe")));
+		}));
 		this.choice_craft$updateButton();
+		if (furnaceHandler) {
+			this.choice_craft$requestRecipeOptions(false);
+		}
 	}
 
 	@Inject(method = "handledScreenTick", at = @At("TAIL"))
 	private void choice_craft$updateButtonTick(CallbackInfo ci) {
+		if (this.handler instanceof AbstractFurnaceScreenHandler) {
+			this.choice_craft$refreshTicks++;
+			if (this.choice_craft$refreshTicks >= choice_craft$REFRESH_INTERVAL) {
+				this.choice_craft$refreshTicks = 0;
+				this.choice_craft$requestRecipeOptions(false);
+			}
+		}
+
 		this.choice_craft$updateButton();
 	}
 
@@ -74,9 +86,17 @@ public abstract class RecipeBookScreenMixin<T extends AbstractRecipeScreenHandle
 			return;
 		}
 
-		boolean visible = this.handler instanceof AbstractFurnaceScreenHandler
-			|| ChoiceCraftClientState.get(this.handler.syncId).options().size() > 1;
+		boolean visible = ChoiceCraftClientState.get(this.handler.syncId).options().size() > 1;
 		this.choice_craft$recipeButton.visible = visible;
 		this.choice_craft$recipeButton.active = visible;
+	}
+
+	@Unique
+	private void choice_craft$requestRecipeOptions(boolean openAfterResponse) {
+		if (openAfterResponse) {
+			ChoiceCraftClientState.requestOpen(this.handler.syncId);
+		}
+
+		ClientPlayNetworking.send(new RequestChoiceRecipeOptionsPayload(this.handler.syncId));
 	}
 }
